1da9d32cbe35223b5fdae16d4893b38348d1fc68
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-04 16:13:14 -0500

    add static serve

--- .nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Navigation } from '../../components/Navigation.vue'
-
-export const LazyNavigation = import('../../components/Navigation.vue' /* webpackChunkName: "components/Navigation" */).then(c => c.default || c)
